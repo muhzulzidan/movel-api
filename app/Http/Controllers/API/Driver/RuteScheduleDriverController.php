5022b940a19739bb8d20cf16a3dc3824740b633e
Shym2501 <sulthannurya@gmail.com>
2023-04-23 10:03:20 +0000

    create fitur buat dan update rute jadwal driver

---
+++ app/Http/Controllers/API/Driver/RuteScheduleDriverController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\API\Driver;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DriverDepartureResource;
+use App\Models\Driver;
+use App\Models\DriverDeparture;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RuteScheduleDriverController extends Controller
+{
+
+    // Fungsi untuk menambah atau mengubah jadwal berangkat
+    public function store_update_rute_jadwal(Request $request)
+    {
+        // Validasi inputan form rute dan jadwal
+        $request->validate([
+            'kota_asal_id' => 'required|exists:kota_kabs,id',
+            'kota_tujuan_id' => 'required|exists:kota_kabs,id',
+            'date_departure' => 'required|date',
+            'time_departure' => 'required|date_format:H:i',
+        ]);
+
+        // Mengambil data user yang sudah login
+        $user = Auth::user();
+
+        // Mengambil data driver berdasarkan user_id
+        $driver = Driver::where('user_id', $user->id)->first();
+
+        // Mengambil data jadwal berangkat driver berdasarkan id driver
+        $driver_departure = DriverDeparture::where('driver_id', $driver->id)->first();
+
+        // Jika driver telah mengatur rute dan jadwal berangkat
+        if ($driver_departure) {
+            //Data akan di update
+            $driver_departure->update([
+                'kota_asal_id' => $request->kota_asal_id,
+                'kota_tujuan_id' => $request->kota_tujuan_id,
+                'date_departure' => $request->date_departure,
+                'time_departure' => $request->time_departure,
+            ]);
+            $rute_jadwal = $driver_departure;
+            $rute_jadwal['message'] = 'Rute dan jadwal berhasil diperbarui';
+        } else {
+            // Jika driver baru pertama kali mengatur rute dan jadwal
+            // Data akan ditambahkan
+            $rute_jadwal = DriverDeparture::create([
+                'driver_id' => $driver->id,
+                'kota_asal_id' => $request->kota_asal_id,
+                'kota_tujuan_id' => $request->kota_tujuan_id,
+                'date_departure' => $request->date_departure,
+                'time_departure' => $request->time_departure,
+            ]);
+            $rute_jadwal['message'] = 'Rute dan jadwal berhasil diatur';
+        }
+
+        // Mengembalikan respon JSON
+        return new DriverDepartureResource($rute_jadwal);
+
+    }
+}
