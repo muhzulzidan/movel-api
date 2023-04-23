210ceb28bb284592f7aa8b98f421e9a02086d5e8
Shym2501 <sulthannurya@gmail.com>
2023-04-23 10:12:32 +0000

    fix fitur master kota kabupaten

--- app/Http/Controllers/API/MasterData/KotaKabController.php
+++ app/Http/Controllers/API/MasterData/KotaKabController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API\MasterData;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\KotaKabResource;
 use App\Models\KotaKab;
+use Illuminate\Http\Request;
 
 class KotaKabController extends Controller
 {
@@ -17,4 +19,24 @@ class KotaKabController extends Controller
             'data' => $kotaKab,
         ]);
     }
+
+    // Fungsi untuk search Kota Kabupaten
+    public function search_kota_kab(Request $request)
+    {
+        // Mendapatkan kata kunci pencarian dari query parameter 'q'
+        $searchTerm = $request->query('q');
+
+        // Jika kata kunci pencarian tidak diisi, tampilkan semua kota
+        if (empty($searchTerm)) {
+            $kotaKabs = KotaKab::orderBy('id')->get();
+        } else {
+            // Jika kata kunci pencarian diisi, filter kota asal berdasarkan nama kota yang cocok dengan kata kunci pencarian
+            $kotaKabs = KotaKab::where('nama_kota', 'like', '%' . $searchTerm . '%')->get();
+        }
+
+        // Mengembalikan data kota dengan API Resource
+        return KotaKabResource::collection($kotaKabs);
+
+    }
+
 }
